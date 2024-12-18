7c32f77f70d65cdd8a44458cf72e32b0c13f3ff9
Shubham <shubh622005@gmail.com>
2024-12-18 19:24:36 +0530

    fix: url

--- src/components/Shader.tsx
+++ src/components/Shader.tsx
@@ -27,7 +27,7 @@ export default function ShaderGenerator() {
 
     try {
       const response = await fetch(
-        " https://invideo-assignment-1.onrender.com/api/generate_content",
+        "https://invideo-assignment-1.onrender.com/api/generate_content",
         {
           method: "POST",
           headers: {
