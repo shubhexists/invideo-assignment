49e186fe81e6cb7dd29ee06c153c5e0d20098412
shubhexists <shubh622005@gmail.com>
2024-12-18 17:25:27 +0530

    fix: added wasm to git for deployment

---
+++ src/wasm/backend_bg.wasm.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const add: (a: number, b: number) => number;
+export const subtract: (a: number, b: number) => number;
+export const multiply: (a: number, b: number) => number;
+export const divide: (a: number, b: number) => number;
+export const calculate: (a: number, b: number) => [number, number, number];
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
